6c476391d9588017092058931d947dc7748d09bb
joe mckinney <mckinney.a.joe@gmail.com>
2017-10-10 15:11:45 -0700

    add the entire project to a repository, i hadnt planned on saving it because i didnt think id be able to do this on my own

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $('#input').submit(function(event) {
+// business logic
+function stripSpace() {
+  return sentence.replace(/\s/g,'');
+}
+
+function reverse(remix) {
+  return remix.split('').reverse().join('');
+}
+
+    var sentence = $('#sentence').val();
+    var letterNumber = parseInt(sentence.length / 2);
+    var beginning = stripSpace(sentence).charAt(letterNumber);
+    var letters = sentence.charAt(sentence.length-1).toUpperCase() + sentence.charAt(0).toUpperCase();
+    // console.log(sentence);
+    var remix = beginning + sentence + letters;
+    alert(reverse(remix));
+    event.preventDefault();
+  });
+});
+// user logic
+
+
+
+// alert(letterNumber);
+// console.log(stripSpace(sentence));
+// console.log(beginning);
